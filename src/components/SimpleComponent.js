f71fc397d99718720e04e1762cc6642c237edfa7
yatsun <yatsunlam25@gmail.com>
2021-11-10 00:06:42 -0500

    commit tree 14c4764f653fc880725c02cb238b3e9fec7fdbf9

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,26 @@
 // Code SimpleComponent Here
+import React, {Component} from 'react'
+
+class SimpleComponent extends Component {
+    state = {
+        mood: "happy"
+    }
+
+    handleClick = () => {
+        let currentMood = this.state.mood === "happy" ? "sad" : "happy"
+        this.setState({
+            mood: currentMood
+        })
+    }
+
+    render() {
+        return <div onClick={this.handleClick}>{this.state.mood}</div>
+    }
+}
+
+
+
+
+
+
+export default SimpleComponent
